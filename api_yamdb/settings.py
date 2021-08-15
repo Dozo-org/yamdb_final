cda708fd1424b5a257e58bf2bf6db05f57cece94
Роман Рогачев <rogachew.r@yandex.ru>
2021-08-15 14:45:03 +0300

    Debug: False

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -7,10 +7,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '178.154.224.251', 'localhost', 'back-prakt.tk', 'www.back-prakt.tk'
+    '178.154.224.251'
 ]
 
 
